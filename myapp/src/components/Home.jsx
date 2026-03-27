4930efcd8307d433f201b1f250ee2c7542b35ac9
arun <allamarunkumar@gmail.com>
2026-03-27 23:06:16 +0530

    added home component and Installed mdbootstrap

---
+++ myapp/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <React.Fragment>
+        <div className="container">
+            <p className="h1 text-center text-secondary">Home component</p>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Home
